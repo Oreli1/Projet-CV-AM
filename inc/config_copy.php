189e92a8e712d57003c31a692c99eeaf2f5199e6
Aurelien <aurelien.mojak@gmail.com>
2017-11-10 15:06:57 +0100

    update

---
+++ inc/config_copy.php
@@ -0,0 +1,11 @@
+<?php
+// Données de configutation
+$config = array(
+  'DB_HOST'=>'',
+  'DB_USER'=>'',
+  'DB_PASSWORD'=>'',
+  'DB_DATABASE'=>'',
+  'MAIL_HOST' => '',
+  'MAIL_USERNAME' => '',
+  'MAIL_PASSWORD' => ''
+);
---
+++ public/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <title>@M Developper & Webdesigner</title>
+    <!-- SEO -->
+<meta name="title" content="@M Developper & Webdesigner">
+<meta name="description" content="@M Developper & Webdesigner">
+
+<!-- font awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+<!-- CSS Bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
+<!-- CSS JQuery UI-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css">
+<!-- mon CSS -->
+<link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <head>
+      <nav>
+        <span>@M</span>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">IT Skills</a></li>
+          <li><a href="">Professional</a></li>
+          <li><a href="">Portfolio</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </nav>
+      <header>
+        <h1>Aurelien MOJAK</h1>
+          <h2>WEB DEVELOPPER & DESIGNER </h2>
+          <img src="img/am.jpg" alt="photo am" height="400" width="400">
+          <div class="pitch">
+            <p>Utque aegrum corpus quassari etiam levibus solet offensis, ita animus eius angustus et tener, quicquid increpuisset, ad salutis suae dispendium existimans factum aut cogitatum, insontium caedibus fecit victoriam luctuosam.</p>
+          </div>
+      </header>
+      <main>
+        <div id"itSkills">
+          <h2>IT SKILLS</h2>
+        </div>
+      </main>
+    </head>
+    <main>
+    </main>
+    <footer>
+    </footer>
+  </body>
+</html>
